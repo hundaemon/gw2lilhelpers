5352d3be93c87efc0aa5ab2dbe17290daf90f4a1
Laura <l.lubenow@web.de>
2024-09-04 20:41:09 +0200

    ApplicationSetup

---
+++ src/main/java/com/hundae/gw2lilhelpers/Gw2lilhelpersApplication.java
@@ -0,0 +1,13 @@
+package com.hundae.gw2lilhelpers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gw2lilhelpersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gw2lilhelpersApplication.class, args);
+	}
+
+}
